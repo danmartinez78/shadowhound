41d27315ff4c60337fabc39ebb397e869da75ab0
Developer <developer@shadowhound.local>
2025-10-28 02:05:14 +0000

    bringup: install RViz configs (config/rviz/*.rviz) so launch can load custom view

--- src/shadowhound_bringup/setup.py
+++ src/shadowhound_bringup/setup.py
@@ -19,6 +19,10 @@ setup(
             os.path.join("share", package_name, "config"),
             glob(os.path.join("config", "*.yaml")),
         ),
+        (
+            os.path.join("share", package_name, "config", "rviz"),
+            glob(os.path.join("config", "rviz", "*.rviz")),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
